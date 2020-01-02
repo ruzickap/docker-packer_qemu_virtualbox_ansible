cc50c445e963b7942a2aaf84fa1c6fc172af79f3
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-01-02 21:11:06 +0100

    Improve startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # https://github.com/boxboat/fixuid
-eval $( fixuid -q )
+eval "$( fixuid -q )"
 
 if [ -n "$PACKER_RUN_TIMEOUT" ]; then
-  timeout --preserve-status --foreground $PACKER_RUN_TIMEOUT /usr/local/bin/packer "$@"
+  timeout --preserve-status --foreground "$PACKER_RUN_TIMEOUT" /usr/local/bin/packer "$@"
 else
   /usr/local/bin/packer "$@"
 fi
